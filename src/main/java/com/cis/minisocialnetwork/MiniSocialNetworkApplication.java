25199fbf2a07082f0eee094767c967921cb20d07
Ruslan Sungatullin <sunruslan331@gmail.com>
2020-02-13 18:35:50 +0300

    initial commit

---
+++ src/main/java/com/cis/minisocialnetwork/MiniSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.cis.minisocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cis/minisocialnetwork/MiniSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cis.minisocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
